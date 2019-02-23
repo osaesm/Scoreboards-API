e9b57598f05d637542bbecb1b2e8797c07832598
Osama <osamabinesmail@gmail.com>
2019-02-23 13:02:46 -0600

    something broken

--- scraper.py
+++ scraper.py
@@ -1,23 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import time
 
 driver = webdriver.Chrome(executable_path='/home/osama-laptop/chromedriver')
 
 
-def espn_nba_scraper(date, team_id):
+def one_team_with_date(date, team_id):
     driver.get("http://www.espn.com/nba/scoreboard/_/date/" + date)
     try:
         driver.find_element_by_xpath("html/body/div[@id='global-viewport']/section/section/div/section[@class='col-b']/div/div[@id='events']/article[@data-awayid='" + team_id + "']/div/section/a[@name='&lpos=nba:scoreboard:boxscore']").click()
-        return_val = "Found"
     except:
         try:
             driver.find_element_by_xpath("html/body/div[@id='global-viewport']/section/section/div/section[@class='col-b']/div/div[@id='events']/article[@data-homeid='" + team_id + "']/div/section/a[@name='&lpos=nba:scoreboard:boxscore']").click()
-            return_val = "Found"
         except:
-            return_val = None
+            driver.close()
+            return None
+    time.sleep(5)
+    game_id = driver.current_url[driver.current_url.find(('gameId=')) + 7:]
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    print(soup.prettify())
+    box_score = {'gameId': int(game_id)}
     driver.close()
-    return return_val
+    return box_score
 
 date = "20190221"
 team_id = "5"
-print(espn_nba_scraper(date, team_id))
+print(one_team_with_date(date, team_id))
