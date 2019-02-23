d9064b31cc508f81fd03c53107dd82a757b164ef
Osama <osamabinesmail@gmail.com>
2019-02-23 02:20:20 -0600

    reaching individual games

--- scraper.py
+++ scraper.py
@@ -3,9 +3,21 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path='/home/osama-laptop/chromedriver')
 
-date = "20190221"
-sport = "nba"
-team_id = "21"
 
-driver.get("http://www.espn.com/" + sport + "/scoreboard/_/date/" + date)
-driver.find_element_by_xpath("html/body/div[@id='global-viewport']/section/section/div/section[@class='col-b']/div/div[@id='events']/article[@data-awayid='" + team_id + "']").click()
+def espn_nba_scraper(date, team_id):
+    driver.get("http://www.espn.com/nba/scoreboard/_/date/" + date)
+    try:
+        driver.find_element_by_xpath("html/body/div[@id='global-viewport']/section/section/div/section[@class='col-b']/div/div[@id='events']/article[@data-awayid='" + team_id + "']/div/section/a[@name='&lpos=nba:scoreboard:boxscore']").click()
+        return_val = "Found"
+    except:
+        try:
+            driver.find_element_by_xpath("html/body/div[@id='global-viewport']/section/section/div/section[@class='col-b']/div/div[@id='events']/article[@data-homeid='" + team_id + "']/div/section/a[@name='&lpos=nba:scoreboard:boxscore']").click()
+            return_val = "Found"
+        except:
+            return_val = None
+    driver.close()
+    return return_val
+
+date = "20190221"
+team_id = "5"
+print(espn_nba_scraper(date, team_id))
